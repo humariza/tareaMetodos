db1d8328196ef5dba5c5d37d42564831f3862af8
Humberto <humbertoarizah@gmail.com>
2018-11-02 12:06:38 -0500

    se corrige algo para buscar la columna con datos malignso y benignos que habia quedado mal y tambien se crean dos arreglos con los indices de estos datos

--- PCR.py
+++ PCR.py
@@ -3,7 +3,7 @@ import matplotlib.pylab as plt
 
 datos= np.genfromtxt('WDBC.dat',delimiter=',',usecols=(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31))
 
-datosByM=np.genfromtxt('WDBC.dat',delimiter=",",usecols=2)
+datosByM=np.genfromtxt('WDBC.dat',delimiter=",",usecols=1, dtype='|U16')
 
 #MATRIZ DE COVARIANZA HECHA EN S6C2
 def matrizCovarianza(datos):
@@ -30,3 +30,17 @@ for i in range(len(eigenvalues)):
 #PUNTO 1 D
 print ('Los dos valores mas importantes corresponden a los autovalores mas grandes que son los que contienen mas correlacion que son:')
 print (eigenvalues[0],'\n y',eigenvalues[1])
+
+#punto 2D 
+#se buscan los indices malos y buenos
+buenos=[]
+malos=[]
+#Recorre todos los datos de tipo y busca B o M que son los unicos dos valores existentes y guarda la posicion en el tipo de dato para poder hacer la separacion
+for indice in range(len(datosByM)):
+   
+    if (datosByM[indice]=="B"):
+        buenos.append(indice)
+    else:
+        malos.append(indice)
+
+print(buenos)
