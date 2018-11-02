627ee7b0c77b520b410ed187abdc3de22971a4e7
Humberto <humbertoarizah@gmail.com>
2018-11-02 16:35:33 -0500

    se anaden unas cositas a furier, falta terminar del 3.4 en adelante

---
+++ Fourier.py
@@ -0,0 +1,49 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft, fftfreq, ifft
+from scipy.interpolate import interp1d
+
+
+#importando set de datos
+signal=np.genfromtxt("signal.dat", delimiter=',')
+incompletos=np.genfromtxt("incompletos.dat", delimiter=',')
+
+incompletosx= incompletos[:,0]
+incompletosy= incompletos[:,1]
+senalx= signal[:,0]
+senaly= signal[:,1]
+
+
+#graficas
+plt.figure()
+plt.plot(senalx,senaly)
+plt.title('Señal')
+plt.savefig("ArizaHumberto_signal.pdf")
+
+N=len(senalx)
+#fourier
+def fourier(senalx, senaly,N): 
+    sumaTotal=np.zeros(N)
+    for i in range(N):
+        for j in range(N):
+            sumaTotal[i]+=senaly[j]*np.exp(-1j*2*np.pi*j*(i/N))
+    return sumaTotal/N
+
+paso=senalx[1]-senaly[0]
+#obteniendo la frecuencia de los datos
+frecuencia=fftfreq(N,paso)
+print('use fftfreq')
+
+fouriersignal= fourier(senalx,senaly,N);
+print(fouriersignal)
+plt.figure()
+plt.plot(frecuencia,fouriersignal)
+plt.title("Fourier Signal") 
+plt.xlabel('Frecuencia')
+plt.ylabel('Transformada de Fourier Discreta')
+# plt.show()
+plt.savefig("ArizaHumberto_TF.pdf")
+
+
+
