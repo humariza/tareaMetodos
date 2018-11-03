ead7030fa6c8ce0008bb1601f0fded6a2088ae98
Humberto <humbertoarizah@gmail.com>
2018-11-02 21:02:55 -0500

    se anade el punto de 2 de fourier 2d falta la escala

--- Fourier2D.py
+++ Fourier2D.py
@@ -6,3 +6,12 @@ from scipy.fftpack import fft, fftfreq, ifft, fft2, ifft2, fftshift, ifftshift
 #Importando imagen
 porFiltrar= plt.imread('arbol(1).png')
 
+x,y=porFiltrar.shape
+fourier2d=fft2(porFiltrar)
+centrada = abs(fftshift(fourier2d))
+
+plt.figure()
+plt.title('TF Arbol')
+plt.imshow(np.log(centrada))
+#esto deberia tener una escala o alg asi respecto a los colores pero estoy sin tiempo
+plt.savefig('ArizaHumberto_FT2D.pdf')
